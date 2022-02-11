cbb6bea1b95bad9aabd50c850bef6c57ac3f2a6b
MatthewHDemon <danielmateopatarroyomolina@gmail.com>
2022-02-11 00:09:31 -0500

    Updates

---
+++ precache-manifest.3e8ecc7cf2be7d68ddc47db00ae325e0.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d8ea700d99d5a58452151581b098a0",
+    "url": "/La_pi-a_el-ctrica/index.html"
+  },
+  {
+    "revision": "bfcbb8b6d08a03a4fcc0",
+    "url": "/La_pi-a_el-ctrica/static/css/main.611fac48.chunk.css"
+  },
+  {
+    "revision": "b2946b47171cc416982f",
+    "url": "/La_pi-a_el-ctrica/static/js/2.d0c8a62f.chunk.js"
+  },
+  {
+    "revision": "bfcbb8b6d08a03a4fcc0",
+    "url": "/La_pi-a_el-ctrica/static/js/main.2f9b4140.chunk.js"
+  },
+  {
+    "revision": "6193f76169d4b9f5b83e",
+    "url": "/La_pi-a_el-ctrica/static/js/runtime~main.65b90103.js"
+  },
+  {
+    "revision": "410ae4b8ff67128f757868801a17bd9f",
+    "url": "/La_pi-a_el-ctrica/static/media/beatlesshirtblack.410ae4b8.png"
+  },
+  {
+    "revision": "2a03cdb5826397535924491a451bfe81",
+    "url": "/La_pi-a_el-ctrica/static/media/beatlesshirtblue.2a03cdb5.png"
+  },
+  {
+    "revision": "fdd246b124f63ce35d503695c3259dc8",
+    "url": "/La_pi-a_el-ctrica/static/media/beatlesshirtred.fdd246b1.png"
+  },
+  {
+    "revision": "a507a7182159b900afdd872aa7084078",
+    "url": "/La_pi-a_el-ctrica/static/media/beatlesshirtwhite.a507a718.png"
+  },
+  {
+    "revision": "b557a5de62e86afe1684d05d95d04494",
+    "url": "/La_pi-a_el-ctrica/static/media/beatlesshirtyellow.b557a5de.png"
+  },
+  {
+    "revision": "d085c09aabf5feeaa8ac462b85db3a72",
+    "url": "/La_pi-a_el-ctrica/static/media/bonhamblack.d085c09a.png"
+  },
+  {
+    "revision": "211ba5bcd429ab79102d618e2f969005",
+    "url": "/La_pi-a_el-ctrica/static/media/bonhamblue.211ba5bc.png"
+  },
+  {
+    "revision": "b5a3b78d5e207a15c7ce1771fa274031",
+    "url": "/La_pi-a_el-ctrica/static/media/bonhamred.b5a3b78d.png"
+  },
+  {
+    "revision": "cf47d93b2c567ed89f853ed38689d0a8",
+    "url": "/La_pi-a_el-ctrica/static/media/bonhamwhite.cf47d93b.png"
+  },
+  {
+    "revision": "211ba5bcd429ab79102d618e2f969005",
+    "url": "/La_pi-a_el-ctrica/static/media/bonhamyellow.211ba5bc.png"
+  },
+  {
+    "revision": "f0d850c89576bc7fc2842d95e22a6aa6",
+    "url": "/La_pi-a_el-ctrica/static/media/foto1.f0d850c8.jpg"
+  },
+  {
+    "revision": "ab0aa1f87e946ea7f2b83f3e19a0a676",
+    "url": "/La_pi-a_el-ctrica/static/media/foto2.ab0aa1f8.jpg"
+  },
+  {
+    "revision": "019e7012c6e3d614a468754b3e9b189d",
+    "url": "/La_pi-a_el-ctrica/static/media/foto3.019e7012.jpg"
+  },
+  {
+    "revision": "82c2cb6aefdcdbe644d75a316762e27b",
+    "url": "/La_pi-a_el-ctrica/static/media/jimyshirtblack.82c2cb6a.png"
+  },
+  {
+    "revision": "f48ab1c22eb63bab1705e1c846c88fce",
+    "url": "/La_pi-a_el-ctrica/static/media/jimyshirtblue.f48ab1c2.png"
+  },
+  {
+    "revision": "2176df1fba6531fb1c4b0d4e5e385a4f",
+    "url": "/La_pi-a_el-ctrica/static/media/jimyshirtred.2176df1f.png"
+  },
+  {
+    "revision": "01a27533635f0ed1dcc016c226c0a897",
+    "url": "/La_pi-a_el-ctrica/static/media/jimyshirtwhite.01a27533.png"
+  },
+  {
+    "revision": "a518482057c9b3e04a58960369e25b2c",
+    "url": "/La_pi-a_el-ctrica/static/media/jimyshirtyellow.a5184820.png"
+  },
+  {
+    "revision": "b987ced76cc57aabc0968ed115050d66",
+    "url": "/La_pi-a_el-ctrica/static/media/kurtshirtblack.b987ced7.png"
+  },
+  {
+    "revision": "4e13afe452a40d811e00b35ab66c16b3",
+    "url": "/La_pi-a_el-ctrica/static/media/kurtshirtblue.4e13afe4.png"
+  },
+  {
+    "revision": "8992d928c4c66e357130e99eb7cc83f0",
+    "url": "/La_pi-a_el-ctrica/static/media/kurtshirtred.8992d928.png"
+  },
+  {
+    "revision": "c1d5065a43b98d8cb63492cbefcc182b",
+    "url": "/La_pi-a_el-ctrica/static/media/kurtshirtwhite.c1d5065a.png"
+  },
+  {
+    "revision": "2e0bb7bac6c44af8e3c289c902c8e85a",
+    "url": "/La_pi-a_el-ctrica/static/media/kurtshirtyellow.2e0bb7ba.png"
+  },
+  {
+    "revision": "f779018dc98cc63cf2feb3a77c30c61b",
+    "url": "/La_pi-a_el-ctrica/static/media/logo.f779018d.gif"
+  },
+  {
+    "revision": "26762fb5dbb75e133fb71c847cde2cb9",
+    "url": "/La_pi-a_el-ctrica/static/media/ozzyshirtblack.26762fb5.png"
+  },
+  {
+    "revision": "7e69f22a0999c982995550a3c322536c",
+    "url": "/La_pi-a_el-ctrica/static/media/ozzyshirtblue.7e69f22a.png"
+  },
+  {
+    "revision": "37d0e21514cfb426b6b291bd7ae6b9df",
+    "url": "/La_pi-a_el-ctrica/static/media/ozzyshirtred.37d0e215.png"
+  },
+  {
+    "revision": "c45a7349bec95ae1503951ff6ff68c29",
+    "url": "/La_pi-a_el-ctrica/static/media/ozzyshirtwhite.c45a7349.png"
+  },
+  {
+    "revision": "646adbf4f2389db5287ee16f79f3979d",
+    "url": "/La_pi-a_el-ctrica/static/media/ozzyshirtyellow.646adbf4.png"
+  }
+]);
